df1690f27cde40df1d7f22ddba56751070ed18b3
wangaevans <wangaevans84@gmail.com>
2024-10-29 09:48:53 +0300

    +vercel json file

--- src/controllers/stkController.ts
+++ src/controllers/stkController.ts
@@ -21,7 +21,7 @@ const handleStkPush = async (req: RequestExtended, res: Response) => {
     PartyA: phone,
     PartyB: process.env.MPESA_BUSINESS_SHORT_CODE,
     PhoneNumber: phone,
-    CallBackURL: "https://wangaevans.com/",
+    CallBackURL: "https://mydomain.com/pat",
     AccountReference: "BuySasa online shop",
     TransactionDesc: "Payment",
   };
